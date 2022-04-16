e433e4a5146b0bf76262c5aa46cfcd47867c75a9
Artem Abramov <tematibr@gmail.com>
2022-04-16 18:03:10 +0300

    Modifications to strip interface and prepare for moonspeak

---
+++ javascript/examples/grapheditor/www/js/Moonspeak.js
@@ -0,0 +1,70 @@
+/**
+ * Constructs a new moonspeak editor
+ */
+MoonspeakEditor = function(chromeless, themes, model, graph, editable)
+{
+	mxEventSource.call(this);
+    this.preinit();
+    this.editorUi = new EditorUi(new Editor(chromeless, themes, model, graph, editable));
+	this.init();
+};
+
+/**
+ * MoonspeakEditor inherits from mxEventSource
+ */
+mxUtils.extend(MoonspeakEditor, mxEventSource);
+
+/**
+ * ===============================================================
+ * Place MoonspeakEditor functionality below
+ */
+
+/**
+ * This is called before EditorUi.init() and Editor.init()
+ * Specifically for hacks that add behaiviour and then call the base function
+ */
+MoonspeakEditor.prototype.preinit = function()
+{
+
+    // hide the left sidebar
+    EditorUi.prototype.splitSize = 0;
+    EditorUi.prototype.hsplitPosition = 0;
+
+    // hide the footer at bottom of page
+    EditorUi.prototype.footerHeight = 0;
+
+    // scrollbars must be enabled at all times so iframes do not reload
+    Graph.prototype.defaultScrollbars = true;
+
+    // this function must be overriden to retain scrollbar styling
+	let mxGraphView_validateBackgroundStyles = mxGraphView.prototype.validateBackgroundStyles;
+    mxGraphView.prototype.validateBackgroundStyles = function()
+    {
+        // here "this" is an mxGraphView instance
+		mxGraphView_validateBackgroundStyles.apply(this, arguments);
+        this.graph.container.classList.add("styled-scrollbars");
+    }
+
+};
+
+/**
+ * This is called after EditorUi.init()
+ * Specifically for hacks that drive change in EditorUi
+ */
+MoonspeakEditor.prototype.init = function()
+{
+    // hide the right sidebar
+    this.editorUi.toggleFormatPanel(false);
+
+    // hide the left sidebar
+    this.editorUi.hsplitPosition = 0;
+    this.editorUi.refresh();
+
+    // make scrollbars less visible
+};
+
+
+/**
+ * ===============================================================
+ * Place prototype overrides below
+ */
